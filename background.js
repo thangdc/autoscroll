ba10f6b608e4bde10a18fec25c8af7b7b5703498
thangdc <thang@thangdc.com>
2019-04-10 09:12:43 +0700

    Add files via upload

--- background.js
+++ background.js
@@ -22,3 +22,24 @@ chrome.runtime.onInstalled.addListener(function() {
 		}]);
 	});
 });
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+	
+	var timeout = 25;
+	var shortcut = 's';
+	
+	chrome.storage.sync.get('timeout', function(data) {
+		timeout = data.timeout;
+	});
+	chrome.storage.sync.get('shortcut', function(data) {
+		shortcut = data.shortcut;
+	});
+	
+	chrome.tabs.executeScript(
+		tabId,
+		{	
+			code: 'ThangDC = window.ThangDC || {}; if(ThangDC.isStart == false) { ThangDC.isStart = true; } else { ThangDC.isStart = false; }; ThangDC.timeout = '+ timeout +'; ThangDC.shortcut = \''+ shortcut +'\'; function start(){ ThangDC.isStart = true; if(typeof (ThangDC.timer) === \'undefined\') { ThangDC.timer = setInterval(function(){ var body = document.body, html = document.documentElement; var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight); console.log(document.documentElement.clientHeight); if ((document.documentElement.clientHeight + document.documentElement.scrollTop) < height) { if(ThangDC.isStart) { window.scrollTo(0, document.documentElement.scrollTop+1); } else { stop(); } } else { stop(); } }, ThangDC.timeout); } } function stop(){ clearInterval(ThangDC.timer); ThangDC.timer = undefined; ThangDC.isStart = false; } document.onkeypress = function(event){ if(event.which == ThangDC.shortcut.charCodeAt(0)) { if(ThangDC.isStart) { stop(); } else { start(); } } }'
+		}
+	);		
+	
+})
--- popup.js
+++ popup.js
@@ -1,40 +1,22 @@
 let startEle = document.getElementById('btnStart');
 let timeoutEle = document.getElementById('tbxTimeout');
-let shortcutEle = document.getElementById('tbxShortcut');
+//let shortcutEle = document.getElementById('tbxShortcut');
 
 var isStart = false;
 var timeout = 25;
-var shortcut = 's';
+var shortcut = 'z';
+
+chrome.storage.sync.set({shortcut: shortcut});
 
 chrome.storage.sync.get('timeout', function(data) {
 	timeoutEle.value = data.timeout;
+	timeout = data.timeout;
 });
 
 chrome.storage.sync.get('shortcut', function(data) {
-	shortcutEle.setAttribute('value', data.shortcut);
+	//shortcutEle.setAttribute('value', data.shortcut);
+	shortcut = data.shortcut;
 });
-
-startEle.onclick = function(element) {
-    
-	if(isStart == true){
-		isStart = false;
-	}
-	else {
-		isStart = true;
-	}
-	
-	timeout = timeoutEle.value;
-	shortcut = shortcutEle.value;
-	
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(
-			tabs[0].id,
-			{	
-				code: 'ThangDC = window.ThangDC || {}; if(ThangDC.isStart) { ThangDC.isStart = false } else { ThangDC.isStart = true; }; ThangDC.timeout = '+ timeout +'; ThangDC.shortcut = \'' + shortcut + '\'; ThangDC.isPause = false; if(typeof (ThangDC.timer) === \'undefined\') { ThangDC.timer = setInterval(function(){ if ((window.innerHeight + document.documentElement.scrollTop) < document.body.clientHeight) { if(ThangDC.isStart) { if(ThangDC.isPause == false) { window.scrollTo(0, document.documentElement.scrollTop+1); } } else { clearInterval(ThangDC.timer); ThangDC.timer = undefined; ThangDC.isStart = false; ThangDC.isPause = false; } } else { clearInterval(ThangDC.timer); ThangDC.timer = undefined; ThangDC.isStart = false; ThangDC.isPause = false; } }, ThangDC.timeout); } document.onkeypress = function(event){ if(event.which == ThangDC.shortcut.charCodeAt(0)) { if(ThangDC.isPause) { ThangDC.isPause = false; } else { ThangDC.isPause = true; } } }'
-			}
-		);
-	});
-};
 
 timeoutEle.onchange = function(element) {
 	timeout = element.target.value;
@@ -49,7 +31,7 @@ timeoutEle.onchange = function(element) {
 	});
 }
 
-shortcutEle.onchange = function(element) {
+/*shortcutEle.onchange = function(element) {
 	shortcut = element.target.value;
 	chrome.storage.sync.set({shortcut: shortcut});
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -60,4 +42,4 @@ shortcutEle.onchange = function(element) {
 			}
 		);
 	});
-}
+}*/
